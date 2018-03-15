0a019da205653800a6d9d92f19eb1e4c59eb5686
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-03-15 10:50:31 +0900

    fake 슬라이드 화면 구현

--- src/components/main/home.vue
+++ src/components/main/home.vue
@@ -1,28 +1,28 @@
 <template>
-  <main role="main">
+  <main role="main" class="container">
     <div id="home-slide" class="carousel slide text-center" data-ride="carousel">
       <ol class="carousel-indicators">
-        <li data-target="#home-slide" v-bind:data-slide-to="item.id" v-for="(item) in slideLists" v-bind:key="item.id" class="active"></li>
+        <li data-target="#home-slide" v-bind:data-slide-to="item.id" v-for="(item, index) in slideLists" :key="item.id" :class="{'active': index === 0}"></li>
       </ol>
       <div class="carousel-inner">
-        <div class="carousel-item" v-for="item in slideLists" v-bind:key="item.id">
-          <img v-bind:src="item.imageUrl" alt="First slide">
+        <div class="carousel-item" v-for="(item, index) in slideLists" :key="item.id" :class="{'active': index === 0}">
+          <img :src="item.imageUrl" class="" :alt="item.alt">
           <div class="container">
             <div class="carousel-caption text-left">
-              <h1>{{item.title}}</h1>
+              <h1>{{item.title}}}</h1>
               <p>{{item.description}}</p>
-              <p><a href="#">상세보기</a></p>
+              <p><a :href="item.url">보러가기</a></p>
             </div>
           </div>
         </div>
       </div>
       <a class="carousel-control-prev" href="#home-slide" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+        <span class="sr-only">이전</span>
       </a>
       <a class="carousel-control-next" href="#home-slide" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+        <span class="sr-only">다음</span>
       </a>
     </div>
   </main>
@@ -55,11 +55,6 @@
   }
 </script>
 <style>
-  .carousel-inner img {
-    max-width: 100%;
-    width:100%;
-    height: 100%;
-  }
   .carousel-control-prev-icon,
   .carousel-control-next-icon {
     height: 100px;
@@ -71,14 +66,14 @@
   {
     background-image: none;
     content: '>';
-    font-size: 55px;
+    font-size: 75px;
     color: red;
   }
 
   .carousel-control-prev-icon:after {
     background-image: none;
     content: '<';
-    font-size: 55px;
+    font-size: 75px;
     color: red;
   }
 </style>
