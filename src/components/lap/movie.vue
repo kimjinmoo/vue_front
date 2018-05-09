80e92d3c831c1fa097abae932b4d3430c16893bc
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-05-09 11:04:17 +0900

    1. api 수정

--- src/components/lap/movie.vue
+++ src/components/lap/movie.vue
@@ -4,12 +4,16 @@
              :show="showDismissibleAlert"
              @dismissed="showDismissibleAlert=false">{{message}}</b-alert>
     <b-button v-on:click="setMyLocation">위치 찾기</b-button>
-    <p>lat : {{center.lat}} / long : {{center.lng}}</p>
+    <p>lat : {{center.lat}} / lng : {{center.lng}}</p>
     <!--:dragend="myPosition"-->
     <!--@center_changed="updateCenter"-->
     <GmapMap :center="center"
              :zoom="zoom"
-             style="min-width: 300px; min-height: 400px">
+             style="min-width: 300px; min-height: 400px"
+             ref="mapRef"
+             @drag="onDrag"
+             @dragend="onDragend"
+    >
       <GmapMarker
           :key="m.id"
           v-for="(m) in markers"
@@ -17,9 +21,15 @@
           :title="m.storeName"
           :clickable="true"
           :draggable="false"
-      />
+          :InfoWindow="m.storeName"
+          @click="onMarker(m.storeName, m.position)"
+      >
+        <gmap-info-window :position="m.position">
+          롯데시네마 : {{m.storeName}}
+        </gmap-info-window>
+      </GmapMarker>
       <gmap-info-window :position="myPosition">
-        Here
+        현재 나의 위치!!
       </gmap-info-window>
     </GmapMap>
   </div>
@@ -27,6 +37,9 @@
 <script>
   var MobileDetect = require('mobile-detect'),md = new MobileDetect(navigator.userAgent);
   import axios from 'axios'
+  // vue google map
+  // ref : https://developers.google.com/maps/documentation/javascript/events?hl=ko
+  // ref : https://github.com/heavyy/vue2-google-maps/blob/master/API.md
 
   export default {
     name : "movie",
@@ -41,12 +54,26 @@
           lat : 37.5665,
           lng : 126.9780
         },
-        zoom : 7,
+        zoom : 3,
         message : "",
         showDismissibleAlert : false
       }
     },
     methods : {
+      onDrag() {
+        console.log("onDrag...");
+      },
+      onDragend() {
+        this.$refs.mapRef.$mapPromise.then((map) => {
+          this.center.lat =  map.getCenter().lat();
+          this.center.lng =  map.getCenter().lng();
+        });
+        console.log("drag call");
+      },
+      onMarker(storeName, position) {
+        this.center = position;
+        this.zoom = 18;
+      },
       updateCenter(center) {
         this.center = {
           lat: center.lat(),
@@ -64,32 +91,41 @@
         if(navigator.geolocation){
           this.$getLocation()
           .then(coordinates => {
-            console.log("lat : " + coordinates.lat);
-            console.log("lng : " + coordinates.lng);
             this.myPosition.lat = coordinates.lat;
             this.myPosition.lng = coordinates.lng;
             this.center.lat = coordinates.lat;
             this.center.lng = coordinates.lng;
             this.zoom = 17;
+            this.$parent.showLoading(false);
           });
         } else {
           this.message = "Web에서 지원하지 않습니다.";
           this.center.lat = 0;
           this.center.lng = 0;
+          this.$parent.showLoading(false);
         }
+
       }
 
     },
     beforeCreate() {
+      this.$parent.showLoading(true);
       axios.get("https://conf.grepiu.com/sample/crawler/lotteCineLocale")
       .then((response) => {
-        this.markers = response.data;
+        let map = [];
+        for(var inx in response.data) {
+          let item = response.data[inx];
+          let lat = item.location == null?0:item.location.y;
+          let lng = item.location == null?0:item.location.x;
+          map.push({"position" : {"lng" : lng, "lat" : lat}, "storeName" : item.storeName})
+        }
+        this.markers = map;
       })
       .catch((e)=>{
         console.log(e);
       })
     },
-    mounted() {
+    created() {
       if(md.is("iPhone")) {
         this.showDismissibleAlert = true;
         this.message = "아이폰 설정 >> 개인 정보 보호 >> 위치 서비스 >> Safari 웹 사이트 >> 안함-> 앱을 사용하는 동안 체크";
